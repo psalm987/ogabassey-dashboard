6e5c8c52446f210aa6de6fc17d2d8e0bdc967a22
Samuel Seibidor <samrythmn@gmail.com>
2023-03-17 18:31:44 +0100

    fix: Test dummy

--- src/pages/api/product/dummy.ts
+++ src/pages/api/product/dummy.ts
@@ -6,5 +6,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse>
 ) {
-  res.status(200).json({ msg: "Dummy successful", success: true });
+  res.status(200).json({ msg: "Dummy successful!", success: true });
 }
