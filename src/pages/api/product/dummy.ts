dbf0c20c89f3d90167cca9382979fd8fa676996a
Samuel Seibidor <samrythmn@gmail.com>
2023-03-18 15:45:28 +0100

    fix: connet database

--- src/pages/api/product/dummy.ts
+++ src/pages/api/product/dummy.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import { createProduct, searchProducts } from "@db/utils/products";
 import type { NextApiRequest, NextApiResponse } from "next";
+import connectDb from "@db/config";
+
+connectDb();
 
 export default async function handler(
   req: NextApiRequest,
