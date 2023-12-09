9cd332f67d75660e7ad91cb44257fe3a4291b81f
Samuel Seibidor <samrythmn@gmail.com>
2023-12-09 23:16:10 +0100

    feat: Add webhook endpoint

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(`verify token: ${req.query["hub.verify_token"]}`);
+  console.log(`body: ${req.body}`);
+  if (req.query["hub.verify_token"] === process.env.VERIFY_TOKEN!) {
+    const body = JSON.parse(req.body);
+    if (body.field !== "messages") {
+      // not from the messages webhook so dont process
+      return res.status(400);
+    }
+  }
+  res.status(200).json({ name: "John Doe" });
+}
