6c0c9dbd99da070152f8cfe0579ba3d1ff7f9a41
Samuel Seibidor <samrythmn@gmail.com>
2023-12-10 11:48:24 +0100

    feat: Reply messages from webhook

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,33 +1,121 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { sendTextMessage } from "src/util/api/whatsapp";
 
-type Data = {
-  name: string;
+type MessageBase = {
+  from: string;
+  id: string;
+  timestamp: string;
+  type?: "text";
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const body = req.body;
-  console.log(`verify token: ${req.query["hub.verify_token"]}`);
-  //   console.log(`body: ${JSON.stringify(body, null, 2)}`);
+type ContactExtentionType = "HOME" | "WORK";
 
-  switch (req.method) {
-    case "POST":
-      if (body.field !== "messages") {
-        // not from the messages webhook so dont process
-        return res.status(400);
-      }
-      console.log("entry: ", body?.entry);
-    case "GET":
-      if (
-        req.query["hub.verify_token"] === process.env.VERIFY_TOKEN! &&
-        req.query["hub.mode"] === "subscribe"
-      ) {
-        return res.status(200).send(req.query["hub.challenge"]);
-      } else {
-        return res.status(400);
-      }
-    default:
-      return res.status(200).json({ name: "John Doe" });
-      break;
+type ContactExtentionAddress = {
+  city: string;
+  country: string;
+  country_code: string;
+  state: string;
+  street: string;
+  type: ContactExtentionType;
+  zip: string;
+};
+
+type ContactExtentionEmail = {
+  email: string;
+  type: ContactExtentionType;
+};
+
+type ContactExtentionName = {
+  formatted_name: string;
+  first_name: string;
+  last_name: string;
+  middle_name: string;
+  suffix: string;
+  prefix: string;
+};
+type ContactExtentionOrg = {
+  company: string;
+  department: string;
+  title: string;
+};
+type ContactExtentionPhone = {
+  phone: string;
+  wa_id: string;
+  type: ContactExtentionType;
+};
+type ContactExtentionUrl = {
+  url: string;
+  type: ContactExtentionType;
+};
+
+type ContactExtention = {
+  addresses: ContactExtentionAddress[];
+  birthday: string;
+  emails: ContactExtentionEmail;
+  name: ContactExtentionName;
+  org: ContactExtentionOrg;
+  phones: ContactExtentionPhone[];
+  urls: ContactExtentionUrl[];
+};
+
+type WebhookData = {
+  object: "whatsapp_business_account";
+  entry: [
+    {
+      id: string;
+      changes: [
+        {
+          value: {
+            messaging_product: "whatsapp";
+            metadata: {
+              display_phone_number: string;
+              phone_number_id: string;
+            };
+          };
+          field: "messages" | string;
+        }
+      ];
+    }
+  ];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const body: WebhookData = req.body;
+    console.log(`verify token: ${req.query["hub.verify_token"]}`);
+    //   console.log(`body: ${JSON.stringify(body, null, 2)}`);
+
+    switch (req.method) {
+      case "POST":
+        const change = body.entry[0].changes?.[0] || {};
+        if (change?.field !== "messages") {
+          // not from the messages webhook so dont process
+          return res.status(400);
+        }
+        change?.value?.metadata?.phone_number_id &&
+          (await sendTextMessage(
+            "Welcome to Ogabassey chatbot!",
+            change?.value?.metadata?.phone_number_id
+          ));
+      case "GET":
+        if (
+          req.query["hub.verify_token"] === process.env.VERIFY_TOKEN! &&
+          req.query["hub.mode"] === "subscribe"
+        ) {
+          return res.status(200).send(req.query["hub.challenge"]);
+        } else {
+          return res.status(400);
+        }
+      default:
+        return res.status(200).json({ name: "John Doe" });
+        break;
+    }
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({ error: e });
   }
 }
---
+++ src/util/api/whatsapp.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const whatsappAPIversion = "v17.0";
+
+const whatsappSenderPhoneNumber = process.env.PHONE_NUMBER_ID!;
+
+const whatsappAxios = axios.create({
+  baseURL: `https://graph.facebook.com/${whatsappAPIversion}/`,
+  headers: {
+    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+  },
+});
+
+export const registerToCloudAPI = async (pin: string) => {
+  const pattern = /\d{6}/;
+  const match = pin.match(pattern);
+  if (!(match && pin === match[0])) {
+    throw Error("Pin should be of 6 digits");
+  }
+  return await whatsappAxios.post(`${whatsappSenderPhoneNumber}/messages`, {
+    messaging_product: "whatsapp",
+    pin,
+  });
+};
+
+export const sendTextMessage = async (message: string, to: string) => {
+  return await whatsappAxios.post(`${whatsappSenderPhoneNumber}/messages`, {
+    messaging_product: "whatsapp",
+    to,
+    text: {
+      body: message,
+    },
+  });
+};
+
+export const markMessageRead = async (
+  message_id: string,
+  sender_id: string
+) => {
+  return await whatsappAxios.post(`${sender_id}/messages`, {
+    messaging_product: "whatsapp",
+    status: "read",
+    message_id,
+  });
+};
