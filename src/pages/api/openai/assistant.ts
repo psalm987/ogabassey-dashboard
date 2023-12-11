ec3b30ee23a31f8cb87a72212c38ef7036522937
Samuel Seibidor <samrythmn@gmail.com>
2023-12-11 19:02:53 +0100

    feat: add assistant test endpoint

---
+++ src/pages/api/openai/assistant.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { startConversation } from "src/util/api/ogabassey-assistant";
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+        switch (req.method) {
+            case "POST":
+                const body = req.body;
+                const { message } = body || {}
+                if (!message) {
+                    throw new URIError()
+                }
+                const completion = await startConversation(
+                    message
+                )
+                return res.status(200).json({ result: completion });
+            default:
+                return res.status(405).json({ msg: "Invalid method", success: false });
+        }
+    } catch (error) {
+        console.error(error);
+        if (error instanceof URIError) {
+            return res
+                .status(400)
+                .json({ msg: "Bad search parameters", success: false });
+        }
+        return res
+            .status(500)
+            .json({ msg: "Internal Server Error", success: false });
+    }
+}
